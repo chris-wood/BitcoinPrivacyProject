585e959ea058d3c4bd97d0c7fa7cb5cf2234de62
chris-wood <christopherwood07@gmail.com>
2014-02-25 12:04:09 -0800

    Integrated message format notes

--- boomerang/code/scripts/badcircuit.py
+++ boomerang/code/scripts/badcircuit.py
@@ -1,29 +1,32 @@
 import math
 import sys
 
-n = int(sys.argv[1])
-d = int(sys.argv[2])
-bad = int(sys.argv[3])
-print n, d, bad
+def badCircuitProb(n, d, bad):
+	# do the computation
+	total = 0
+	for i in range(0, d):
+		prod = float(bad) / (n - 1 - i)
+		innerProd = 1.0
+		if i >= 1:
+			for j in range(0, i):
+				innerProd = innerProd * (1.0 - (float(bad) / (n - 1 - j)))
+			total = total + (prod * innerProd)
+		else:
+			total = prod
+	print str(n) + "," + str(d) + "," + str(bad) + "," + str(total)
 
-total = 0
-for i in range(0, d):
-	# print("i = " + str(i))
-	prod = float(bad) / (n - 1 - i)
-	# print("i = " + str(i) + ", prod = " + str(prod))
-	innerProd = 1.0
-	if i >= 1:
-		for j in range(0, i):
-			# print("j = " + str(j))
-			innerProd = innerProd * (1.0 - (float(bad) / (n - 1 - j)))
-		total = total + (prod * innerProd)
-	else:
-		total = prod
+def main():
+	# parse cmd lines
+	nlow = int(sys.argv[1])
+	nhigh = int(sys.argv[2])
+	nskip = int(sys.argv[3])
+	dlow = int(sys.argv[4])
+	dhigh = int(sys.argv[5])
 
-	# print "\n",
-	# print(prod)
-	# print(innerProd)
-	# print(total)
+	for n in range(nlow, nhigh + 1, nskip):
+		for d in range(dlow, dhigh + 1):
+			for bad in range(1, n/2 + 1):
+				badCircuitProb(n, d, bad)
 
-print "\n",
-print(total)
+if __name__ == "__main__":
+	main()
