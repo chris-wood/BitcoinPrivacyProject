687402c53de6bbe48af073a03f2c9ff42a85279d
chris-wood <christopherwood07@gmail.com>
2014-03-05 18:44:06 -0800

    Updated the simulation scripts

--- boomerang/code/src/runSimulations.py
+++ boomerang/code/src/runSimulations.py
@@ -7,6 +7,9 @@ import glob
 # Declare variables for the delta+1 and delta+2 output
 filedump = ""
 
+# Compile!
+p = subprocess.Popen('javac -cp "jyaml-1.3.jar:." *.java', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
 if (len(sys.argv) != 2):
 	print "Usage: python runSimulations.py fileListFile"
 else:
@@ -16,7 +19,7 @@ else:
 		fname = str(fname.strip())
 		print >> sys.stderr, "Running: " + fname
 		prefix = str(os.path.basename(fname).split(".")[0]) # drop extension
-		p = subprocess.Popen('java -Xmx12g -cp "jyaml-1.3.jar;." Boomerang ' + str(fname), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		p = subprocess.Popen('java -Xmx12g -cp "jyaml-1.3.jar:." Boomerang ' + str(fname), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		fout = open(prefix + ".out", 'w')
 		for line in p.stdout.readlines():
 			line = line.strip()
